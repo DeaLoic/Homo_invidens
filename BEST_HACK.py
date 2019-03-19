449748662a057c3df0906cba889f6c6ce4e15d89
EgorGrishin <grischin.egor2001@yandex.ru>
2019-03-19 18:47:03 +0300

    Correct formules, but dont work(?)

--- BEST_HACK.py
+++ BEST_HACK.py
@@ -1,32 +1,50 @@
 from math import sqrt
-from input_data import m, g, H, V_start, x_aim, z_aim, F_from_V, Wind_x, Wind_z, delta_t
+import pandas as pd
+from scipy.interpolate import interp1d
 
+
+m = float(input("input m: "))
+g = 9.81
 
 x = 0
-y = H
+y = float(input("input h: "))
 z = 0
 
-speed_x = V_start
+speed_x = float(input("input v0: "))
 speed_y = 0
 speed_z = 0
 
-t = 0
+x_aim = float(input("x target: "))
+z_aim = float(input("z target: "))
 
-trajectory = pd.DataFrame({'Time': [], 'X': [], 'Y': [], 'Z': [],
-                           'Speed_x': [], 'Speed_y': [], 'Speed_z': []})
-trajectory.loc[0] = t, x, y, z, speed_x, speed_y, speed_z
-i = 1
+t = 0
+delta_t = float(input("delta t: "))
+
+wind_csv = pd.read_csv("Wind.csv")
+f_csv = pd.read_csv("F.csv")
+
+
+v_wind_h_x = interp1d(wind_csv["Height (m)"], wind_csv["Wx (m/s)"], "nearest", fill_value="extrapolate")
+v_wind_h_z = interp1d(wind_csv["Height (m)"], wind_csv["Wz (m/s)"], "nearest", fill_value="extrapolate")
+f_aer_v = interp1d(f_csv["V(m/s)"], f_csv["F(N)"], "nearest", fill_value="extrapolate")
+
+
+trajectory = pd.DataFrame(columns=["t", "x", "y", "z", "speed_x", "speed_y", "speed_z"])
+trajectory.loc[0] = [t, x, y, z, speed_x, speed_y, speed_z]
+N = 1
 
 while y > 0:
-    speed_air_x = Wind_x(y) - speed_x
+    speed_air_x = v_wind_h_x(y) - speed_x
     speed_air_y = - speed_y
-    speed_air_z = speed_wind_z(y) - speed_z
+    speed_air_z = v_wind_h_z(y) - speed_z
 
     speed_air_full = sqrt(speed_air_x * speed_air_x + speed_air_y * speed_air_y + speed_air_z * speed_air_z)
-    
-    speed_x_next = speed_x - force_coef * speed_air_full * speed_air_x * delta_t / m
-    speed_y_next = speed_y - g * delta_t + force_coef * speed_air_full * speed_air_y * delta_t / m
-    speed_z_next = speed_z - force_coef * speed_air_full * speed_air_z * delta_t / m
+
+    f_cur_v = f_aer_v(speed_air_full)
+
+    speed_x_next = speed_x - (f_cur_v * speed_air_x / speed_air_full) * speed_air_full * delta_t / m
+    speed_y_next = speed_y - (g + (f_cur_v * speed_air_y / speed_air_full) * speed_air_full / m) * delta_t
+    speed_z_next = speed_z - (f_cur_v * speed_air_z / speed_air_full) * speed_air_full * delta_t / m
 
     x += (speed_x_next + speed_x) / 2 * delta_t
     y += (speed_y_next + speed_y) / 2 * delta_t
@@ -37,20 +55,19 @@ while y > 0:
     speed_z = speed_z_next
 
     t += delta_t
-    
-    trajectory[N] = [t, x, y, z, speed_x, speed_y, speed_z]
+
+    trajectory.loc[N] = [t, x, y, z, speed_x, speed_y, speed_z]
     N += 1
 
-trajectory[N - 1][2] = 0
+delta_x = x_aim - trajectory.loc[N - 1][1]
+delta_z = z_aim - trajectory.loc[N - 1][3]
 
-# Узнаём требуемую точку попадания и соответственно сдвигаем всю траекторию по горизинтали
-x_aim = float(input())
-z_aim = float(input())
-delta_x = x_aim - trajectory[N - 1][1]
-delta_z = z_aim - trajectory[N - 1][3]
 
-for i in range(N):
-    trajectory[i][1] += delta_x
-    trajectory[i][3] += delta_z
+trajectory.loc[N-1][2] = 0
+trajectory["x"] += delta_x
+trajectory["z"] += delta_z
 
-    
+trajectory.to_csv('Trajectory.csv')
+
+print("Angle: 0\nX0: {}\nZ0: {}".format(trajectory.loc[0]["x"], trajectory.loc[0]["z"]))
+
